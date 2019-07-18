3028822ed326dd343d6c107adc5f4d64ddf1f7c0
CWWhitney <whitney.cory@gmail.com>
2019-07-18 17:06:41 +0800

    temp changes

--- Temp.R
+++ Temp.R
@@ -1,2 +1,16 @@
+participants_data<-read.csv("participants_data.csv")
+library(dplyr)
+names(participants_data)
+aca_work_filter<-select(participants_data, academic_parents,
+                        working_hours_per_day)
 
-#Heading 1 -----
+names(aca_work_filter)
+
+?ethnobotanydata #the '?' symbol opens the help file..
+names(ethnobotanydata)
+str(ethnobotanydata)
+head(ethnobotanydata)
+
+?str
+?head
+?names
